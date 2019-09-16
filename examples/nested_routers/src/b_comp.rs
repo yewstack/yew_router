afb4f6d6a264de084dd4383f530abe0996792ffb
Henry Zimmerman <zimhen7@gmail.com>
2019-09-16 16:24:36 -0400

    add nested router example

---
+++ examples/nested_routers/src/b_comp.rs
@@ -0,0 +1,48 @@
+use yew::prelude::*;
+use yew_router::render::component;
+use yew_router::route;
+use yew_router::{Route, Router};
+
+use crate::page_not_found::PageNotFound;
+
+pub struct BComp {}
+
+pub enum Msg {}
+
+impl Component for BComp {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        BComp {}
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        true
+    }
+
+    fn destroy(&mut self) {
+        log::info!("BComp destroyed")
+    }
+}
+
+impl Renderable<BComp> for BComp {
+    fn view(&self) -> Html<Self> {
+        html! {
+            <>
+                <div>
+                    { "I am the B component"}
+                </div>
+                <div>
+                    <Router>
+                        <Route matcher=route!("/b/{*}") render=component::<PageNotFound>() />
+                    </Router>
+                </div>
+            </>
+        }
+    }
+}
---
+++ examples/nested_routers/src/main.rs
@@ -0,0 +1,56 @@
+use yew::{html, Component, ComponentLink, Html, Renderable, ShouldRender};
+use yew_router::{Router, Route, route, render::component};
+use yew_router::components::RouterButton;
+use crate::page_not_found::PageNotFound;
+use crate::a_comp::AComp;
+use crate::b_comp::BComp;
+
+
+mod page_not_found;
+mod a_comp;
+mod b_comp;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+
+fn main() {
+    web_logger::init();
+    yew::start_app::<Model>();
+}
+
+struct Model { }
+
+enum Msg {
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Model { }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+}
+
+impl Renderable<Model> for Model {
+    fn view(&self) -> Html<Self> {
+        html! {
+            <>
+                <RouterButton text="A" link="/a/" />
+                <RouterButton text="B" link="/b/" />
+
+                <Router>
+                    <Route matcher=route!("/a") render=component::<AComp>() />
+                    <Route matcher=route!("/b") render=component::<BComp>() />
+                    <Route matcher=route!("{*}") render=component::<PageNotFound>() />
+                </Router>
+            </>
+        }
+    }
+}
+
