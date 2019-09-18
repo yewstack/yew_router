b5c62ffce87b96499b2d130f1a9b8d9e9665327d
Henry Zimmerman <zimhen7@gmail.com>
2019-09-18 16:51:22 -0400

    add rustdoc lib description

--- yew_router/src/lib.rs
+++ yew_router/src/lib.rs
@@ -1,5 +1,39 @@
 #![recursion_limit = "128"]
 //! Provides routing faculties for the Yew web framework.
+//!
+//! ## Contents
+//! This crate consists of multiple types, some independently useful on their own,
+//! that are used together to facilitate routing within the Yew framework.
+//! Among them are:
+//! * RouteService - Hooks into the History API and listens to `PopStateEvent`s to respond to users clicking the back/forwards buttons.
+//! * RouteAgent - A singleton agent that owns a RouteService that provides an easy place for other components and agents to hook into it.
+//! * Router - A component that can choose one of its nested child Routes to render based on the URL.
+//! * Route - A component that supplies a matching condition and a render target to the Router.
+//! * Matcher - An enum that determines if a URL will match a given route. A custom syntax for declaring these exists with the `route!()` macro, or you can use a Regex, or supply your own matcher.
+//! * RouteButton & RouteLink - Wrapper components around buttons and anchor tags respectively that allow users to change the route.
+//! * RouteInjector - A component that allows the injection of the current route into its nested children.
+//!
+//! ## State and Aliases
+//! Because the History API allows you to store data along with a route string,
+//! most types have at type parameter that allows you to specify which type is being stored.
+//! As this behavior is uncommon, aliases are provided to remove the need to specify the storage
+//! type you likely aren't using.
+//!
+//! If you do indeed want to specify the data to be stored in the History API consider the following:
+//! * The aliased types are typically named the same as their implementations, but are importable from a module higher up the hierarchy. You will have to look around for them in the docs as they aren't reexported at the highest level like their respective aliases.
+//! * You probably want to wrap your type in an `Option` and alias your type to make specifying it easier.
+//! * There are varying, mostly undocumented, limits to the maximum size of objects you can store in the History API across different browsers (firefox appears to be the lowest bar at 640kb). Keeping this in mind for cross-browser compatibility is a must.
+//!
+//! ## Orphaning
+//! Currently it is possible to "orphan" components in Yew. This happens when a component doesn't display anymore,
+//! but isn't entirely cleaned up, causing it to still exist and respond to messages sent to it by agents.
+//! This can have negative effects on your program when enough of these accumulate, if for example,
+//! one of the orphaned components makes fetch requests at part of its component lifecycle.
+//!
+//! This library is particularly good at causing this bug to appear.
+//!
+//! To avoid the orphaning problem, it is recommended to only have one top-level Router component in your project for the moment.
+//! Use this Router to collect as much information as needed for any sub-routes, and pass that along to children components via Properties.
 
 #![deny(
     missing_docs,
