4d3ea6c5cc93a10f8f09fb713cfd45885965cba7
Henry Zimmerman <hgzimmerman@users.noreply.github.com>
2019-09-21 21:15:59 -0400

    document the route macro (#50)

--- yew_router/src/lib.rs
+++ yew_router/src/lib.rs
@@ -83,26 +83,131 @@ pub use yew_router_macro::FromCaptures;
 /// The route macro produces a Matcher which can be used to determine if a route string should cause
 /// a section of html or component should render.
 ///
+/// ### Exact Matching
 /// At its simplest, the macro will accept a literal string containing a path, query, and fragment.
 /// Not all parts are required, but the macro will fail if they are specified out of order.
 /// If the route acquired from the browser matches this exactly, the route will match, and the
 /// associated target will be rendered.
 ///
+/// ### Particularities
+/// The route macro is pretty strict in what it allows.
+/// It will reject your matcher string under the following circumstances:
+/// * If `//` is a possible valid match sequence (this has implications for where Optional sections may be used)
+/// * There can only be one `?` character. It denotes the start of the query section. Subsequent queries must begin with `&`.
+/// * Incomplete queries are not allowed. They must follow the form of `<?|&><literal>=<literal|any>`.
 ///
-/// On top of just matching strings literally, match sections, denoted by `{}` can be supplied to
-/// match anything in that section.
+/// ### Any Matching
+/// On top of just matching strings literally, Any sections, denoted by `{}` can be supplied to
+/// match anything in that section. They must match one or more characters to be valid.
+/// Captured values will be available as part of a `Captures` struct if the matching succeeds.
+/// `Captures` is a type alias to `HashMap<&str, String>`.
+/// If you want to specify that a captured section must be a number, you must capture the string,
+/// and then attempt to parse it to your desired numeric type in `from_matches` or `render`.
 ///
-/// If a key starting with a valid rust identifier is supplied between the brackets like
-/// `{key}` then the characters that gets matched by this section will be captured and will become
-/// available in a HashMap returned from the matcher if it succeeds.
+/// Any sections can come in multiple forms:
 ///
-/// If a `*` is contained within the brackets like `{*}`, then the capture section will match across all paths.
+/// * `{}` - Matches non-separator characters until the end of the route or until the next section of exact characters are matched.
+/// * `{key}`- Matches anything, just as above, but stores the captured characters as a String inside a HashMap with the specified name acting as a key.
+/// * `{*}` - Ignore all path separators (`/`), consuming characters until the end of the route or the next section is exactly matched.
+/// * `{*:key}` - Matches as above, but stores the captured characters as a String inside a HashMap.
+/// * `{4}` - Consume the specified number of path separators (`/`) before being allowed to match against a terminating set of characters.
+/// * `{4:key} - Same as above, but stores the captured characters as a String inside a HashMap.
 ///
-/// If a number is specified within the brackets like `{3}`, then that number of path separators (`/`)
-/// must be encountered before that match rule ends. `{1}` is equivalent to `{}`.
+/// There are a rule to remember here:
+/// * Because Any matchers use the subsequent exact section to terminate their search, no Any matchers cannot be next to each other.
+///   * Optional matchers do not adequately separate Any matchers, because they may not match at all, leaving no exact section to separate the Any matchers.
 ///
-/// These `*` and number variants can be combined with the key-capture feature to capture sections to
-/// apply these rules while capturing sections with a named key. This looks like `{*:key}` or `{2:other_key}`.
+/// ### Optional Matching
+/// Optional matchers are denoted by `()` characters. They must contain at least one character.
+/// They either match their contents, or not at all.
+/// Optional matchers cannot opt out of parts of queries, although they can opt out of entire queries.
+///
+/// ### Parser Options
+/// There are currently three options for the parser. One or more may be specified after the
+/// matcher string (delimited by spaces).
+/// * `Strict`
+/// * `CaseInsensitive`
+/// * `Incomplete`
+///
+/// By default, an optimizing step will insert an optional `/` after the path if doing so is valid.
+/// If you want to turn this behavior off, add the word `Strict` after the provided string.
+///
+/// The matcher is case sensitive by default, but that can be disabled by specifying `CaseInsensitive` after the provided string.
+///
+/// The matcher will fail by default if the provided route string isn't completely matched by the matcher.
+/// Specifying `Incomplete` will allow it to succeed, even if the matcher doesn't complete the whole route string.
+///
+///
+/// # Examples
+///
+/// #### Exact match
+/// ```
+///# use yew_router::route;
+/// let matcher = route!("/lorem/ipsum/dolor/sit?amet=consectetur&adipiscing=elit#sed");
+/// assert!(matcher.match_route_string("/lorem/ipsum/dolor/sit?amet=consectetur&adipiscing=elit#sed").is_some());
+/// ```
+///
+/// #### Capture
+/// ```
+///# use yew_router::route;
+/// let matcher = route!("/lorem/ipsum/{value1}/{value2}");
+/// let captures = matcher.match_route_string("/lorem/ipsum/dolor/sit").unwrap();
+/// assert_eq!(captures["value1"], "dolor".to_string());
+/// assert_eq!(captures["value2"], "sit".to_string());
+/// ```
+///
+/// #### Match Many
+/// ```
+/// # use yew_router::route;
+/// let matcher = route!("/lorem/ipsum/{*}");
+/// assert!(matcher.match_route_string("/lorem/ipsum/dolor/sit").is_some());
+/// assert!(matcher.match_route_string("/lorem/ipsum/").is_some());
+///
+/// let matcher = route!("/{*}/dolor/sit");
+/// assert!(matcher.match_route_string("/lorem/ipsum/dolor/sit").is_some());
+/// assert!(matcher.match_route_string("/lorem/dolor/sit").is_some());
+/// assert!(matcher.match_route_string("/dolor/sit").is_none());
+/// ```
+///
+/// #### Match Optional
+/// ```
+///# use yew_router::route;
+/// let matcher = route!("/lorem(/ipsum)");
+/// assert!(matcher.match_route_string("/lorem/ipsum").is_some());
+/// assert!(matcher.match_route_string("/lorem").is_some());
+///
+///
+/// let matcher = route!("/lorem(/ipsum{any})");
+/// let captures = matcher.match_route_string("/lorem/ipsumdolorsit").unwrap();
+/// assert_eq!(captures["any"], "dolorsit".to_string());
+/// ```
+///
+/// #### Trailing Auto-Optional Slash
+/// ```
+///# use yew_router::route;
+/// let matcher = route!("/lorem");
+/// assert!(matcher.match_route_string("/lorem").is_some());
+/// assert!(matcher.match_route_string("/lorem/").is_some());
+///
+/// let matcher = route!("/lorem" Strict);
+/// assert!(matcher.match_route_string("/lorem").is_some());
+/// assert!(matcher.match_route_string("/lorem/").is_none());
+/// ```
+///
+/// #### Case Insensitivity
+/// ```
+///# use yew_router::route;
+/// let matcher = route!("/lorem/ipsum" CaseInsensitive);
+/// assert!(matcher.match_route_string("/loReM/IpSuM").is_some());
+/// ```
+///
+/// #### Incomplete
+/// ```
+///# use yew_router::route;
+/// let matcher = route!("/lorem" Incomplete);
+/// assert!(matcher.match_route_string("/lorem/ipsum").is_some());
+/// ```
+///
 #[cfg(feature = "matchers")]
 #[proc_macro_hack]
 pub use yew_router_macro::route;
