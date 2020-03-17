b95627648b7429aa286de2870f8a6253676b116b
graseggergh <59436493+graseggergh@users.noreply.github.com>
2020-03-17 19:17:45 +0100

    replace `Yew` with `yew-router` (#258)
    Thanks!

--- build.rs
+++ build.rs
@@ -4,9 +4,9 @@ pub fn main() {
     let using_web_sys = cfg!(feature = "web_sys");
     let using_std_web = cfg!(feature = "std_web");
     if using_web_sys && using_std_web {
-        panic!("Yew does not allow the `web_sys` and `std_web` cargo features to be used simultaneously");
+        panic!("yew-router does not allow the `web_sys` and `std_web` cargo features to be used simultaneously");
     } else if !using_web_sys && !using_std_web {
-        panic!("Yew requires selecting either the `web_sys` or `std_web` cargo feature");
+        panic!("yew-router requires selecting either the `web_sys` or `std_web` cargo feature");
     }
 
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
